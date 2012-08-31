5b8bf3c291dfab5ee7458d20107aa9e48e779f80
MrJackson <mrjackson@gmail.com>
2012-08-31 14:23:33 -0400

    modified:   Admin.inc 	modified:   ../www/test.php

--- www/test.php
+++ www/test.php
@@ -1,4 +1,16 @@
 <?php
+        require_once('../Includes/Header.inc');
+        if ($_SESSION['LoggedIn'])
+        {
+                Admin::Status('update');
+        }
+        elseif(!$_SESSION['LoggedIn'])
+        {
+                header('Location: login.php');
+        }
+?>
+
+<?php
 $output = shell_exec('/usr/bin/heyu onstate c12');
 echo "<pre>$output</pre>";
 ?>
