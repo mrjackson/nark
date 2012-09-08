12ecafac147efb11b9e4380fc7d6ba50568f4000
Will Rogers <wrogers@fridge.hsd1.pa.comcast.net>
2012-09-08 08:31:01 +0000

    did some code optimizations; requires testing

--- www/test.php
+++ www/test.php
@@ -1,16 +1,15 @@
 <?php
-        require_once('../Includes/Header.inc');
-        if ($_SESSION['LoggedIn'])
-        {
-                Admin::Status('update');
-        }
-        elseif(!$_SESSION['LoggedIn'])
-        {
-                header('Location: login.php');
-        }
-?>
+	require_once('../Includes/Header.inc');
 
-<?php
-$output = shell_exec('/usr/bin/heyu onstate c12');
-echo "<pre>$output</pre>";
+	if ($_SESSION['LoggedIn'])
+	{
+			Admin::Status('update');
+	}
+	elseif(!$_SESSION['LoggedIn'])
+	{
+			header('Location: login.php');
+	}
+        
+	$output = shell_exec('/usr/bin/heyu onstate c12');
+	echo "<pre>$output</pre>";
 ?>
