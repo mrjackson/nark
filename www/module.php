60e30d694bcabc006516c4d75dc4dc1bcad1251b
MrJackson <mrjackson@gmail.com>
2012-09-29 02:07:21 -0400

    modified:   module.php

--- www/module.php
+++ www/module.php
@@ -14,50 +14,9 @@
 					$Dimmer->SetLevel($_GET['Change']);
 					break;
 			}
-		}
-		elseif ($change > $exist)
-		{
-			$dimchange = $change - $exist;
-			$dimchangeinc = round($dimchange / 6);
-			$return = heyu("bright", $device, $name, $dimchangeinc);
-
-			while (true)
-			{
-				if ($return < $change)
-				{
-					$option = "dim";
-					$return--;
-				}
-				elseif($return > $change)
-				{
-					$option = "bright";
-					$return++;
-				}
-				else
-				{
-					break;
-				}
-
-				heyu($option, $device, $name, "1");
-			}
-		}
-	}
-
-	function heyu($action, $device, $name, $change)
-	{
-		$command = "/usr/bin/heyu $action $device $change";
-		exec($command);
-		if (($action == 'dim') || ($action == 'bright'))
-		{
-			$command = "/usr/bin/heyu dimlevel $device";
-			return exec($command);
-//			echo $return;
-		}
-=======
 			break;
 		case 'Switch':
 			$Switch = new Module($_GET['Module']);
->>>>>>> test
 
 			switch($_GET['Action'])
 			{
